bb094af77ab6b74d8598d445b00b779712cc6e0e
Fábio Valentim <maitabom@gmail.com>
2025-05-11 16:42:39 -0300

    Adicionar classes Transaction e TransactionType
    Foram adicionadas duas novas classes: `Transaction` e `TransactionType`. A classe `Transaction` contém propriedades como `Id`, `Type`, `Name`, `Date` e `Value`, enquanto `TransactionType` é um enum que define dois tipos de transações: `Income` (Receita) e `Expense` (Despesa). Além disso, foram incluídos os namespaces e as diretivas `using` necessárias para o funcionamento do código.

---
+++ Financeiro/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financeiro.Models;
+
+public class Transaction
+{
+    public int Id { get; set; }
+    public TransactionType Type { get; set; }
+    public required String Name { get; set; }
+    public DateTimeOffset Date { get; set; }
+    public Decimal Value { get; set; }
+}
---
+++ Financeiro/Models/TransactionType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financeiro.Models;
+
+public enum TransactionType
+{
+    Income,
+    Expense
+}
