05e162bf324658d57462a0f4b105a855258cfbf7
Fábio Valentim <maitabom@gmail.com>
2025-05-18 16:07:52 -0300

    Adicionar botão para nova transação e atualizar rótulo
    Foi adicionado um botão "+" na tela de lista de transações (TransactionList.xaml) que, ao ser clicado, chama o método `btnAdd_Clicked` no código por trás (TransactionList.xaml.cs) para navegar para a nova página de transação (TransactionNew).
    
    Além disso, a estrutura do arquivo TransactionNew.xaml foi ajustada, incluindo a ordem dos atributos na tag `<ContentPage>`, e o texto do rótulo foi alterado de "Welcome to .NET MAUI!" para "Nova Transação".

--- Financeiro/Views/TransactionList.xaml.cs
+++ Financeiro/Views/TransactionList.xaml.cs
@@ -2,8 +2,16 @@ namespace Financeiro.Views;
 
 public partial class TransactionList : ContentPage
 {
-	public TransactionList()
-	{
-		InitializeComponent();
-	}
+    public TransactionList()
+    {
+        InitializeComponent();
+    }
+
+    private void btnAdd_Clicked(object sender, EventArgs e)
+    {
+        if (Application.Current?.Windows.FirstOrDefault() is { } mainWindow)
+        {
+            mainWindow.Page = new TransactionNew();
+        }
+    }
 }
