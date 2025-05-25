3bc22b0d4bf13f150305cfda52e79a8a6264d7f9
Fábio Valentim <maitabom@gmail.com>
2025-05-25 15:58:58 -0300

    Adiciona validação e novos elementos na tela de transação
    O arquivo `TransactionNew.xaml` foi atualizado com novos elementos de interface, incluindo um rótulo para erros, botões de opção para receita e despesa, e entradas para nome e valor da transação. O botão de salvar agora possui um evento de clique associado.
    
    No arquivo `TransactionNew.xaml.cs`, foram implementados métodos para gerenciar eventos e uma nova função de validação que verifica se os campos estão preenchidos corretamente e se o valor é um número válido. Mensagens de erro são exibidas quando a validação falha.

--- Financeiro/Views/TransactionNew.xaml.cs
+++ Financeiro/Views/TransactionNew.xaml.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Financeiro.Views;
 
 public partial class TransactionNew : ContentPage
@@ -7,8 +9,54 @@ public partial class TransactionNew : ContentPage
 		InitializeComponent();
 	}
 
+    private void ContentPage_Loaded(object sender, EventArgs e)
+    {
+
+    }
+
     private void imaClose_Tapped(object sender, TappedEventArgs e)
     {
 		Navigation.PopModalAsync();
     }
+
+    private void btnSave_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private bool Validate()
+    {
+        bool isValid = true;
+        StringBuilder error = new StringBuilder();
+
+        if (String.IsNullOrEmpty(txtName.Text) || String.IsNullOrWhiteSpace(txtName.Text))
+        {     
+            error.AppendLine("O campo Nome é obrigatório.");
+            isValid = false;
+        }
+
+        if (String.IsNullOrEmpty(txtValue.Text) || String.IsNullOrWhiteSpace(txtValue.Text))
+        {
+            error.AppendLine("O campo Valor é obrigatório.");
+            isValid = false;
+        }
+
+        double result;
+
+        if (!Double.TryParse(txtValue.Text, out result))
+        {
+            error.AppendLine("O campo Valor deve ser um número válido.");
+            isValid = false;
+        }
+
+        if (!isValid)
+        {
+            lblError.IsVisible = true;
+            lblError.Text = error.ToString();
+        }
+
+        return isValid;
+    }
+
+    
 }
