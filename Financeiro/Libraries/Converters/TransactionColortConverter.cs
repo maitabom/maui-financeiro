1d9a5fa6a79dc91eb16b460e35622226e3ccda8d
Fábio Valentim <maitabom@gmail.com>
2025-06-07 15:55:24 -0300

    Adicionar conversores de valor e cor de transação
    Implementados `TransactionValueConverter` e `TransactionColortConverter` para formatar valores e alterar cores de texto com base no tipo de transação. Atualizado `TransactionList.xaml` para integrar esses conversores na interface do usuário.

---
+++ Financeiro/Libraries/Converters/TransactionColortConverter.cs
@@ -0,0 +1,29 @@
+﻿using Financeiro.Models;
+using System.Globalization;
+
+namespace Financeiro.Libraries.Converters;
+
+public class TransactionColortConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is Transaction transaction)
+        {
+            if (transaction.Type == TransactionType.Income)
+            {
+                return Color.FromArgb("#939E5A");
+            }
+            else if (transaction.Type == TransactionType.Expense)
+            {
+                return Color.FromArgb("#FF0000");
+            }
+        }
+
+        return Colors.Black;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Financeiro/Libraries/Converters/TransactionValueConverter.cs
@@ -0,0 +1,30 @@
+﻿using Financeiro.Models;
+using System.Globalization;
+
+namespace Financeiro.Libraries.Converters;
+
+public class TransactionValueConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is Transaction transaction)
+        {
+            var formattedValue = transaction.Value.ToString("C", culture);
+
+            if (transaction.Type == TransactionType.Income)
+            {
+                return $"{formattedValue} ( + )";
+            }
+            else if (transaction.Type == TransactionType.Expense)
+            {
+                return $"{formattedValue} ( - )";
+            }
+        }
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
