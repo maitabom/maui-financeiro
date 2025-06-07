8297701f49e3dde3782fdc1402431fb6da082a01
Fábio Valentim <maitabom@gmail.com>
2025-06-07 16:36:47 -0300

    Adiciona conversores para formatação de transações
    Foram implementados os conversores `TransactionNameTextConverter` e `TransactionNameColorConverter` para formatar o texto e a cor do nome das transações no `TransactionList.xaml`. A cor de fundo do `Border` e o texto do `Label` foram atualizados para utilizar esses novos conversores. Os arquivos correspondentes foram criados para definir a lógica de conversão.

---
+++ Financeiro/Libraries/Converters/TransactionNameColorConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Financeiro.Libraries.Converters;
+
+public class TransactionNameColorConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var random = new Random();
+        var color = String.Format("#{0:X6}", random.Next(0x1000000));
+        return Color.FromArgb(color);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Financeiro/Libraries/Converters/TransactionNameTextConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Financeiro.Libraries.Converters;
+
+public class TransactionNameTextConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var name = value as string;
+
+        return String.IsNullOrEmpty(name) ? String.Empty : name.First().ToString().ToUpper();
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
